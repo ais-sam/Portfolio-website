ebffe6aa0bd938fa20c7d3f77691281501aea935
aissam <aissam.16.25@gmail.com>
2022-06-08 18:37:04 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    container:{
+      center:true,
+      padding:"1rem",
+      screens:{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px"
+      }
+    },
+    extend: {
+      colors:{
+        "pf-grey": "#252525",
+        "pf-black":"#020202",
+        "pf-yellow" : "#FFB800"
+      }
+    },
+  },
+  plugins: [],
+}
